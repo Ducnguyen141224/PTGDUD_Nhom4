c28693a5bec83d7522c72e0a18d6468c10df2b57
Ducnguyen141224 <vanduc141224@gmail.com>
2026-05-20 20:36:19 +0700

    cap nhat mau env mongodb

--- api/debug-env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(_req, res) {
-  res.status(200).json({
-    hasMongoUri: Boolean(process.env.MONGODB_URI),
-    nodeEnv: process.env.NODE_ENV || "",
-    vercelEnv: process.env.VERCEL_ENV || "",
-  });
-}
